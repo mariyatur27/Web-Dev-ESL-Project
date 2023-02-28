c3d8522ccaf087fcd3a4c39155d3555fb264f38a
John Doe <MidKnight8822@gmail.com>
2023-02-27 22:02:28 -0500

    Created search bar for Clubs page - Matthew

--- Template/Impact/assets/js/clubs.js
+++ Template/Impact/assets/js/clubs.js
@@ -11,3 +11,21 @@ async function buildClubBlocks() {
     }
 
 }
+
+// Clubs search bar
+function searchClubs() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("clubSearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("wciClubs");
+    li = ul.getElementsByTagName("li");
+  
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
